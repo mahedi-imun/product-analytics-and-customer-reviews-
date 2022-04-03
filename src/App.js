8e7cfc4ced5b187f754da21fe7111f453fc2457e
mahedi <mahedianik4@gmail.com>
2022-04-04 00:59:13 +0600

    add navbar with react route and , add  home,reviews blogs, dashboard, about,404 , components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Blogs from './components/Blogs/Blogs';
+import Dashboard from './components/Dashboard/Dashboard';
+import Home from './components/Home/Home';
+import Nav from './components/Nav/Nav';
+import NotFound from './components/NotFoundPage/NotFound';
+import Reviews from './components/Reviews/Reviews';
 
 function App() {
   return (
     <div className="App">
-      <h2>hwllo</h2>
+      <Nav></Nav>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/reviews' element={<Reviews/>}></Route>
+        <Route path='/dashboard' element={<Dashboard/>}></Route>
+        <Route path='/blogs' element={<Blogs/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='*' element={<NotFound/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import './Nav.css'
+const Nav = () => {
+    const navigate = useNavigate()
+    return (
+        <nav className='  bg-black border-gray-200 py-6 text-white'>
+            <div className='container flex content-center items-center  justify-between'>
+                <button onClick={() => navigate('/home')} className=' text-2xl ml-12'>Alfa bike</button>
+                <div className='mr-12 nav-items'>
+                    <NavLink className={({ isActive }) => (isActive ? "text-orange-400" : "text-white")} to="/home">Home</NavLink>
+                    <NavLink className={({ isActive }) => (isActive ? "text-orange-400" : "text-white")} to="/reviews"> Reviews</NavLink>
+                    <NavLink className={({ isActive }) => (isActive ? "text-orange-400" : "text-white")} to="/dashboard"> Dash board</NavLink>
+                    <NavLink className={({ isActive }) => (isActive ? "text-orange-400" : "text-white")} to="/blogs"> Blogs</NavLink>
+                    <NavLink className={({ isActive }) => (isActive ? "text-orange-400" : "text-white")} to="/about">About</NavLink>
+                </div>
+
+            </div>
+
+        </nav>
+    );
+};
+
+export default Nav;
