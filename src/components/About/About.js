872ad08b958d787c7ec980c581ea4a14464543aa
mahedi <mahedianik4@gmail.com>
2022-04-04 21:05:42 +0600

    add text about me page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,8 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>this is about</h2>
+            <h2 className='text-center font-black'> about me</h2>
+            <p className='text-center font-serif w-40 m-auto mt-4'> I am  mahaedi imun anik , I am from Bangladesh. I stayed in Lakshmipur with my family- my mother , my sister and my wife. </p>
         </div>
     );
 };
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -6,11 +6,11 @@ const Blogs = () => {
             <h3 className='mt-5 mb-2 font-bold'>WHAT IS CONTEXT API?</h3>
             <p className=' font-serif'>the react context api is a way for a react app to effectively produce gloabl vaiable that can be passes around , this is the alternative to prop drilling or moving props grand parent to child to carent and so pn. react.createContext() is all you need.it returns a consumer and probider.the context api is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application.</p>
             <h3 className='mt-5 mb-2 font-bold'> WHAT IS SIMANTIC TAG?</h3>
-            <p className=' font-serif'>semantic HTML tags allow you to add meaning to your markup so that searce engine  screen readers and web browser can make sense of it. by defult, user agent reads your content it doesn't understand the context and meaning. Semantic HTML tags let you serve structure content to your users, which is espacially important for on-page SEO and accessibility.
-             semantic tags existed in earlier HTML versions, the HTML5 spacifications added several new semantic element to the syntax—both on the block and inline levels. In this article, we'll look into how HTML semantics work and also give you some tips about how to get the most out of the feture.</p>
+            <p className=' font-serif'>semantic html tags allow you to add meaning to your markup so that searce engine  screen readers and web browser can make sense of it. by defult, user agent reads your content it doesn't understand the context and meaning. Semantic html tags let you serve structure content to your users, which is espacially important for on-page SEO and accessibility.
+             semantic tags existed in earlier html versions, the html 5 spacifications added sevaral new semantic element to the syntax both on the block and inline levels.</p>
              <h3 className='mt-5 mb-2 font-bold'> DIFFERENCE BETWEEN BLOCK ELEMENT AND INLINE BLOCK ELEMENTS ??</h3>
-             <p className=' font-serif'> Block elements: they conseme the entire width available irrespective of their suficiency. they always start in a new line and have top and bottom margins. it does not contain any other elements next to it. <br />
-             Inline Elements: inline elements occupy only suficient width required.nnline elements don't start in a new line.inline elements allow other inline elements to sit behind
+             <p className=' font-serif'> block elements: they conseme the entire width available irrespective of their suficiency. they always start in a new line and have top and bottom margins. it does not contain any other elements next to it. <br />
+             inline elements: inline elements occupy only suficient width required.nnline elements don't start in a new line.inline elements allow other inline element to sit beahind
              </p>
         </div>
     );
