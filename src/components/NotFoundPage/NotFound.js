96af472d3d8debd43f39766795fb70931016c82b
mahedi <mahedianik4@gmail.com>
2022-04-04 07:05:25 +0600

    add 404 page design with back home button

--- src/components/NotFoundPage/NotFound.js
+++ src/components/NotFoundPage/NotFound.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import './NotFound.css'
 const NotFound = () => {
+    const navigate = useNavigate()
     return (
-        <div>
-            <h3>not found</h3>
+        <div className=''>
+            <div className='not-found-bg rounded-2xl text-center  '>
+                <button onClick={() => navigate('/home')} type="button" class="text-white bg-gradient-to-r  from-transparent via-bg-transparent hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2  mt-72 border ">go home</button>
+            </div>
         </div>
     );
 };
 
-export default NotFound;<h3>not found</h3>
+export default NotFound; 
